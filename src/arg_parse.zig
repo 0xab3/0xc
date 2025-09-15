40a9676846768b1e410d22cf8ab97ee8390ce02f
shahzad <shahzadakhtar182005@gmail.com>
2025-09-15 22:00:15 +0530

    feat(arg_parse): add parsing for out file name

--- src/arg_parse.zig
+++ src/arg_parse.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
-input_file: []const u8 = undefined,
+
+input_filename: []const u8 = undefined,
+output_filename: []const u8 = undefined,
 const Self = @This();
 pub fn init(self: *Self) void {
     self.* = .{};
@@ -12,10 +14,15 @@ pub fn populate(self: *Self) void {
     var iter = std.process.ArgIterator.init();
     var flag = iter.next();
     var is_file_flag = false;
+    var _output_filename: ?[]const u8 = null;
     while (flag != null) {
-        if (std.mem.eql(u8, flag.?, "-i") == true) {
+        if (std.mem.eql(u8, flag.?, "-i")) {
             const name = iter.next();
-            self.input_file = name.?;
+            self.input_filename = name.?;
+            is_file_flag = true;
+        } else if (std.mem.eql(u8, flag.?, "-o")) {
+            const name = iter.next();
+            _output_filename = name.?;
             is_file_flag = true;
         }
         flag = iter.next();
@@ -24,4 +31,5 @@ pub fn populate(self: *Self) void {
         help();
         std.process.exit(1);
     }
+    self.output_filename = _output_filename orelse "a.out";
 }
