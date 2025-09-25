27d68d4ccfd2bb98b42a29812c499f45d1b7ef96
shahzad <shahzadakhtar182005@gmail.com>
2025-09-25 21:07:56 +0530

    feat(codegen/x64_nasm): compile int literals as procedure arguments

--- src/codegen/x64_nasm_linux.zig
+++ src/codegen/x64_nasm_linux.zig
@@ -6,10 +6,11 @@ const StringBuilder = @import("../string_builder.zig");
 
 program_builder: StringBuilder,
 const Self = @This();
+
 pub fn init(allocator: Allocator) Self {
     return .{ .program_builder = .init(allocator) };
 }
-fn get_inst_based_on_size(size: u32) []const u8 {
+fn get_size_identifier_based_on_size(size: u32) []const u8 {
     return switch (size) {
         1 => "BYTE",
         2 => "WORD",
@@ -19,12 +20,24 @@ fn get_inst_based_on_size(size: u32) []const u8 {
     };
 }
 
+// @UNIMPORTANT(shahzad): yes we are duplicating the code... First defined at  type_check.zig:44
+pub fn get_size_of_int_literal(int_literal: u64) u16 {
+    const n_bits: u64 = std.math.log2(int_literal) + 1;
+    const n_bytes: u16 = std.math.divCeil(u16, @intCast(n_bits), 8) catch unreachable;
+    assert(n_bytes <= 8);
+    return if ((n_bytes & n_bytes - 1) == 0) n_bytes else @as(u16, @intCast(1)) << @intCast(std.math.log2(n_bytes) + 1);
+}
+
 // generate code here and return the ident that specifies the location?
 // for the variable
 // @TODO(shahzad): this is shitty idea but by default we just put anything we've compiled to rax
 pub fn compile_expr(self: *Self, module: *Ast.Module, procedure: *Ast.ProcDef, expr: *const Ast.Expression) ![]u8 {
     switch (expr.*) {
         .LiteralInt => |expr_as_int_lit| {
+            // @TODO(shahzad): use the size identifier
+            const size_ident = get_size_identifier_based_on_size(get_size_of_int_literal(expr_as_int_lit));
+            _ = size_ident;
+
             try self.program_builder.append_fmt("   mov rax, {}\n", .{expr_as_int_lit});
         },
         .NoOp => {},
@@ -33,16 +46,32 @@ pub fn compile_expr(self: *Self, module: *Ast.Module, procedure: *Ast.ProcDef, e
             const stack_offset = stack_var.?.offset;
             try self.program_builder.append_fmt("   mov r8,rbp\n", .{});
             try self.program_builder.append_fmt("   sub r8,{}\n", .{stack_offset});
-            std.log.debug("@TODO!(shahzad): compile_expr for var is not implemented!", .{});
         },
         .Call => |call_expr| {
-            // we don't support any function with arity more than one
+            // @TODO(shahzad)!!!!!: we don't support any function with arity more than one
             assert(call_expr.params.items.len == 1);
             for (call_expr.params.items) |param_expr| {
                 _ = try self.compile_expr(module, procedure, &param_expr);
-                assert(param_expr == .Var);
-                try self.program_builder.append_fmt("   mov rdi, [r8]\n", .{});
-                try self.program_builder.append_fmt("   call {s} wrt ..plt\n", .{call_expr.name});
+                switch (param_expr) {
+                    .Var => {
+                        try self.program_builder.append_fmt("   mov rdi, [r8]\n", .{});
+                    },
+                    .LiteralInt => {
+                        try self.program_builder.append_fmt("   mov rdi, rax\n", .{});
+                    },
+                    else => unreachable,
+                }
+                // @TODO(shahzad): this is a hack cause we can also define our own put char
+                if (module.get_proc_decl(call_expr.name) != null) {
+                    try self.program_builder.append_fmt("   call {s} wrt ..plt\n", .{call_expr.name});
+                } else {
+                    // @NOTE(shahzad): if the proc is not in decl_array that means it is not extern, which means
+                    // that it can only be a defined proc, calling undeclared proc is handled by the parser
+                    assert(module.get_proc_def(call_expr.name) != null);
+
+                    try self.program_builder.append_fmt("   call {s}\n", .{call_expr.name});
+                }
+                if (std.mem.eql(u8, call_expr.name, "putchar")) {} else {}
             }
         },
     }
@@ -106,7 +135,7 @@ pub fn compile_mod(self: *Self, module: *Ast.Module) !void {
     try self.program_builder.append_fmt("section .text\n", .{});
     if (module.has_main_func) {
         try self.program_builder.append_fmt("global main\n", .{});
-        // we linking with crt so we don't care much about anything for now
+        // @NOTE(shahzad): we linking with crt so we don't care much about anything for now
     }
     for (module.proc_decls.items) |*proc_decl| {
         try self.program_builder.append_fmt("extern {s}\n", .{proc_decl.name});
